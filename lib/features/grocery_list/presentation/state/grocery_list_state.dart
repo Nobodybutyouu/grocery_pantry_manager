a913384ed6be98a020b2e6f3c840a5f51fbd6ebb
Nobodybutyouu <margallomarklourince@gmail.com>
2025-11-16 08:09:46 +0800

    Add feature/grocery-list-generator

---
+++ lib/features/grocery_list/presentation/state/grocery_list_state.dart
@@ -0,0 +1,42 @@
+// lib/features/grocery_list/presentation/state/grocery_list_state.dart
+
+import '../../data/models/grocery_item_model.dart';
+
+abstract class GroceryListState {
+  const GroceryListState();
+}
+
+class GroceryListInitial extends GroceryListState {
+  const GroceryListInitial();
+}
+
+class GroceryListLoading extends GroceryListState {
+  const GroceryListLoading();
+}
+
+class GroceryListLoaded extends GroceryListState {
+  final List<GroceryItemModel> items;
+  final int checkedCount;
+  final int uncheckedCount;
+
+  const GroceryListLoaded({
+    required this.items,
+    required this.checkedCount,
+    required this.uncheckedCount,
+  });
+
+  int get totalCount => items.length;
+  double get progress => totalCount > 0 ? checkedCount / totalCount : 0;
+}
+
+class GroceryListError extends GroceryListState {
+  final String message;
+
+  const GroceryListError(this.message);
+}
+
+class GroceryListSuccess extends GroceryListState {
+  final String message;
+
+  const GroceryListSuccess(this.message);
+}
